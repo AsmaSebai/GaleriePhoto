ab11a0dc198a0bd173b5e91d2fde16cba4d2e7bc
AsmaSebai <49121343+AsmaSebai@users.noreply.github.com>
2020-11-29 20:21:35 +0100

    First commit

---
+++ src/app/photo-detail/photo-detail.page.spec.ts
@@ -0,0 +1,28 @@
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { RouterTestingModule } from '@angular/router/testing'; 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { PhotoDetailPage } from './photo-detail.page';
+import { ActivatedRoute } from '-node_modules/@angular/router/router';
+
+describe('PhotoDetailPage', () => {
+  let component: PhotoDetailPage;
+  let fixture: ComponentFixture<PhotoDetailPage>;
+  let route: ActivatedRoute;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PhotoDetailPage ],
+      imports: [IonicModule.forRoot(), RouterTestingModule, HttpClientTestingModule]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PhotoDetailPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
